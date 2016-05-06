d9031bbdb903c9ccee13649e5795e59cc48fa527
pase13voxi <pascal_bechtoldt@web.de>
2016-05-06 15:39:17 +0200

    neustes Update
    Menü wurde verbessert und die Farben wurden angepasst.

--- Teespeicher/app/src/main/java/paseb/teeapp/teespeicher/MainActivity.java
+++ Teespeicher/app/src/main/java/paseb/teeapp/teespeicher/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
+        //TestToast
         Toast toast1 = Toast.makeText(getApplicationContext(), "Kurz vor dem Laden.", Toast.LENGTH_SHORT);
         toast1.show();
         //Settings holen
